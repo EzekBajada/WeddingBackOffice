bc0ea4b7925c96ce8f11a3a9a4070335e747a1c0
Ezekiel Bajada <ezekielbajada@thynksoftware.com>
2022-07-17 18:00:45 +0200

    init design

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <RsvpStatus msg="testing"></RsvpStatus>
+  <RsvpStatus count="0" isAttending="true"></RsvpStatus>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import RsvpStatus from './components/RsvpStatus.vue'
 
 export default {
   components: {
-    // HelloWorld,
     RsvpStatus
   }
 }
--- src/components/RsvpStatus.vue
+++ src/components/RsvpStatus.vue
@@ -1,12 +1,33 @@
 <template>
-    <h1 class="pl-5 text-3xl font-bold underline">{{ msg }}</h1>
+<div class="flex flex-row h-screen">
+    <div class="rounded-lg bg-green-500 h-1/2 w-1/2">
+      {{count}}
+    </div>
+
+    <div class="rounded-lg bg-red-500 h-1/2 w-1/2">
+      {{count}}
+    </div>
+</div>
 </template>
 
 <script>
+
+
 export default {
-  props: {
-    msg: String,
-    count: Number
+  data() {
+    return {
+      rsvp: {
+        count: 1,
+        isAttending: 'My Journey with Vue'
+      }
+    }
+  },
+  created () {
+    if(!this.count || this.count == 0) {
+      this.count = 2;
+      this.isAttending = false;
+      console.log('We have a problem')
+    }
   }
 }
 </script>
