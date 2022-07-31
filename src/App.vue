4fded336f6e311bf076c949468c1ec5a3c0d8199
Ezekiel Bajada <ezek_bajada@hotmail.co.uk>
2022-07-31 16:46:44 +0200

    Added fonts and style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
-  <RsvpStatus rsvp-count="10" :is-attending="true"></RsvpStatus>
+  <div class="flex flex-row p-3">
+    <div class="w-1/2 mr-2">
+      <div>
+        <div class="flex items-center justify-center font-moonBold">
+          <h1>Attending</h1>
+        </div>
+        <RsvpStatus rsvp-count="10" :is-attending="true"></RsvpStatus>
+      </div>
+    </div>
+    <div class="w-1/2 ml-2">
+      <div class="flex items-center justify-center font-moonBold">
+        <h1>Not Attending</h1>
+      </div>
+      <RsvpStatus  rsvp-count="20" :is-attending="false"></RsvpStatus>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/RsvpStatus.vue
+++ src/components/RsvpStatus.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" p-10 rounded-lg text-lg text-white"
-       :class="props.isAttending ? 'bg-green-500' : 'bg-red-500'" >
+  <div class="flex items-center justify-center p-10 rounded-lg text-lg text-white font-moonBold"
+      :class="{'bg-green-500': props.isAttending, 'bg-red-500': !props.isAttending}">
     {{ props.rsvpCount }}
   </div>
 </template>
@@ -11,6 +11,6 @@
   const props = defineProps({
     rsvpCount: Number,
     isAttending: Boolean
-  });  
+  });
 
 </script>
