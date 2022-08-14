4a128b23fcc4186107f024d243d8184e7093424a
Ezekiel Bajada <ezek_bajada@hotmail.co.uk>
2022-08-14 11:08:05 +0200

    updated

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,34 @@
         <div class="flex items-center justify-center font-moonBold">
           <h1>Attending</h1>
         </div>
-        <RsvpStatus :rsvp-count="attending.length" :is-attending="true"></RsvpStatus>
+        <RsvpStatus :rsvp-count="result.attending" :is-attending="true"></RsvpStatus>
       </div>
     </div>
     <div class="w-1/2 ml-2">
       <div class="flex items-center justify-center font-moonBold">
         <h1>Not Attending</h1>
       </div>
-      <RsvpStatus  :rsvp-count="notAttending.length" :is-attending="false"></RsvpStatus>
+      <RsvpStatus :rsvp-count="result.notAttending" :is-attending="false"></RsvpStatus>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import RsvpStatus from './components/RsvpStatus.vue'
 import axios from 'axios'
 
-export default {
-  components: {
-    RsvpStatus
-  },
-  mounted(){
-    var attending;
-    var notAttending;
+    var result = {
+      attending: 0,
+      notAttending: 0
+    }
+
     axios.get(process.env.VUE_APP_BASE_URL + 'Guests', {
       headers : {
         'X-API-Key': process.env.VUE_APP_API_KEY
       }
     })
     .then(response => {
-      attending = response.data.filter(x=> x.isAttending)
-      notAttending = response.data.filter(x=> !x.isAttending)
-      console.log(attending)
-      console.log(notAttending)
+      result.attending = Number.parseInt(response.data.filter(x=> x.isAttending).length);
+      result.notAttending = Number.parseInt(response.data.filter(x=> !x.isAttending).length);
     });
-  }
-}
 </script>
