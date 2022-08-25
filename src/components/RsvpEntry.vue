eb1c2ef72a547c4848eabc4b9e1ebc1da7cd7fbd
Ezubot <ezek_bajada@hotmail.co.uk>
2022-08-25 17:46:40 +0200

    added new icon for delete

--- src/components/RsvpEntry.vue
+++ src/components/RsvpEntry.vue
@@ -1,22 +1,36 @@
 <template>
-    <div class="flex flex-row tablet:w-full laptop:w-1/2 items-center justify-center bg-gray-800 tablet:text-sm laptop:text-lg font-moonBold p-3 rounded-lg text-white">
+    <div class="flex flex-row tablet:w-full laptop:w-1/2 items-center justify-center bg-gray-800 tablet:text-sm laptop:text-lg font-moonBold p-3 rounded-lg"
+        :class="classes">
         <div>
             {{props.name}} {{props.surname}}
         </div>
-        <div class="rounded-lg tablet:ml-2 laptop:ml-10 p-1 tablet:text-sm laptop:text-xl" :class="classes">
-            {{props.isAttending ? 'Attending' : 'Not Attending'}}
-        </div>
-        <div class="rounded-lg tablet:ml-2 laptop:ml-10 p-1 bg-stone-600">
+        <div class="rounded-lg tablet:ml-2 laptop:ml-10 p-1 bg-stone-600 text-white">
             {{props.numberOfGuests}}
+        </div>
+        <div class="pl-10 text-white">
+            <font-awesome-icon class="hover:cursor-pointer" @click="deleteGuest()" icon="fa-solid fa-trash-can" />
         </div>
 </div>
 </template>
 
 <script setup>
     import { defineProps } from 'vue';
+    import axios from 'axios'
     const classes = {
-        'bg-green-500': props.isAttending, 
-        'bg-red-500': !props.isAttending
+        'text-green-500': props.isAttending, 
+        'text-red-500': !props.isAttending
+    }
+
+    function deleteGuest(){
+        axios.post(process.env.VUE_APP_BASE_URL + 'Guests/DeleteGuest', {
+            name: props.name,
+            surname: props.surname} ,{
+            headers : {
+                'X-API-Key': process.env.VUE_APP_API_KEY
+            }}).then(response => {
+                console.log(response)
+                this.$forceUpdate();
+        });
     }
 
     const props = defineProps({
@@ -24,6 +38,6 @@
         surname: Number,
         isAttending: Number,
         numberOfGuests: Number,
-        lastUpdated: Number
+        lastUpdated: Date
     })
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import './index.css'
 
-createApp(App).mount('#app')
+/* add icons to the library */
+library.add(faTrashCan)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
